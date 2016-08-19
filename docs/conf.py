c042d888c235ba947c3f2b60a7231ea7e9261d74
Damien <dgarros@gmail.com>
2016-08-19 14:04:14 -0700

    Docs update

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
@@ -122,7 +122,15 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+# on_rtd is whether we are on readthedocs.org
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -157,6 +165,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_style = 'css/my_theme.css'
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,7 @@
-.. Juniper Automation documentation master file, created by
-   sphinx-quickstart on Fri Aug 19 07:01:07 2016.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to Juniper Automation's documentation!
 ==============================================
+
+Add introductions here
 
 Contents:
 
@@ -14,10 +11,11 @@ Contents:
    ansible
    pyez
    jet
+   contribution
+   documentation
 
 Indices and tables
 ==================
-
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
