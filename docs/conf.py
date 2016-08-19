7b88804dfe085f683d3c1ac483803c2c0210c59d
Damien <dgarros@gmail.com>
2016-08-19 16:44:14 -0700

    Change css position

--- docs/conf.py
+++ docs/conf.py
@@ -134,9 +134,9 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 else:
   html_context = {
     'css_files': [
-        '_static/css/my_theme.css',
         'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
         'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+        '_static/css/my_theme.css',
     ],
   }
 # Theme options are theme-specific and customize the look and feel of a theme
