3e3f59f1a8a28f7b3fad91e8e9b409e84a57230b
pklimai <pklimai@users.noreply.github.com>
2017-10-27 20:24:46 +0300

    Create other_options.py

---
+++ pyez/PyEZ_Cookbook_2017/18-Automated_Network_Testing_with_PyEZ/other_options.py
@@ -0,0 +1,23 @@
+# Same test using direct RPC call
+def check_ospf_full_adjacencies(dev, neighbor_count):
+    full_count = 0
+    rpc_result = dev.rpc.get_ospf_neighbor_information()
+    for element in rpc_result.findall("ospf-neighbor"):
+        if element.findtext("ospf-neighbor-state") == "Full":
+            full_count += 1
+        else:
+            return False
+    return full_count == neighbor_count
+
+# Same test using jxmlease
+import jxmlease
+def check_ospf_full_adjacencies(dev, neighbor_count):
+    full_count = 0
+    parser = jxmlease.EtreeParser()
+    res = parser(dev.rpc.get_ospf_neighbor_information())
+    for neighbor_data in res["ospf-neighbor-information"]["ospf-neighbor"]:
+        if neighbor_data["ospf-neighbor-state"] == "Full":
+            full_count += 1
+        else:
+            return False
+    return full_count == neighbor_count
