ce31dfd37491715c0060d96d9d7f85e8a578b13f
Jessica Garrison <jgarrison@juniper.net>
2017-11-01 23:24:03 -0700

    Fixed pep8

--- pyez/PyEZ_Cookbook_2017/13-Bandwidth_Reservation_for_MPLS_Access_Rings/check_rsvp_interfaces.py
+++ pyez/PyEZ_Cookbook_2017/13-Bandwidth_Reservation_for_MPLS_Access_Rings/check_rsvp_interfaces.py
@@ -1,10 +1,12 @@
-# Copyright 2017, Juniper Networks Pvt Ltd.
-# All rights reserved.
-#!/usr/bin/python 
+#!/usr/bin/python
 
 # purpose is to check the rsvp interface reservation on an ACX device
 
-import os, sys, yaml, glob, errno
+import os
+import sys
+import yaml
+import glob
+import errno
 from pprint import pprint
 from jnpr.junos import Device
 from jnpr.junos.factory.factory_loader import FactoryLoader
@@ -41,34 +43,50 @@ globals().update(FactoryLoader().load(yaml.load(RSVP_INT_yml)))
 
 _rsvpout = "/var/tmp/rsvp.out"
 
+
 def log(text):
 
-        file_log.write(text)
+    file_log.write(text)
+
 
 file_log = open(_rsvpout, "w")
 
+
 def get_rsvp(_IP1):
-	try:
-		_dev = Device(host=_IP1,user=_uname,password=_upass)
-		_dev.open()
-	except Exception as err:
-		print "Can not Connect" + _IP1, err
-		return
-		sys.exit(1)
+    try:
+        _dev = Device(host=_IP1, user=_uname, password=_upass)
+        _dev.open()
+    except Exception as err:
+        print "Can not Connect" + _IP1, err
+        return
+        sys.exit(1)
 
-	rsvp_int_T = Rsvp_int_Table(_dev)
-	rsvp_int_T.get()
-	for rsvp in rsvp_int_T:
-		file_log.write(_IP1 + ";" + rsvp.interface_name + ";" + rsvp.rsvp_status + ";" + rsvp.rsvp_telink_active_reservation + ";" + rsvp.rsvp_telink_subscription + ";" + rsvp.rsvp_telink_available_bandwidth + '\n')
+    rsvp_int_T = Rsvp_int_Table(_dev)
+    rsvp_int_T.get()
+    for rsvp in rsvp_int_T:
+        file_log.write(
+            _IP1 +
+            ";" +
+            rsvp.interface_name +
+            ";" +
+            rsvp.rsvp_status +
+            ";" +
+            rsvp.rsvp_telink_active_reservation +
+            ";" +
+            rsvp.rsvp_telink_subscription +
+            ";" +
+            rsvp.rsvp_telink_available_bandwidth +
+            '\n')
 
-	_dev.close()
+    _dev.close()
+
 
 _directory = os.path.normpath("/PATH/TO/FILE/")
 
 for subdir, dirs, files in os.walk(_directory):
-	for file in files:
-		if file.endswith(".txt"):
-			with open(os.path.join(subdir, file),'r') as _IP1:
-				for line in _IP1:
-					line = line.strip()
-					get_rsvp(line)
+    for file in files:
+        if file.endswith(".txt"):
+            with open(os.path.join(subdir, file), 'r') as _IP1:
+                for line in _IP1:
+                    line = line.strip()
+                    get_rsvp(line)
