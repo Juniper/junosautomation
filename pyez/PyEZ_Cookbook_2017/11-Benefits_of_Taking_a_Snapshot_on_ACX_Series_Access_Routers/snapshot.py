eea6f10a158fba844a3a2b5a1dcabf27448d9dcb
Jessica Garrison <jgarrison@juniper.net>
2017-11-01 23:27:02 -0700

    Fix pep8

--- pyez/PyEZ_Cookbook_2017/11-Benefits_of_Taking_a_Snapshot_on_ACX_Series_Access_Routers/snapshot.py
+++ pyez/PyEZ_Cookbook_2017/11-Benefits_of_Taking_a_Snapshot_on_ACX_Series_Access_Routers/snapshot.py
@@ -1,7 +1,3 @@
-# Copyright 2017, Juniper Networks Pvt Ltd.
-# All rights reserved.
-#!/usr/bin/env python
-
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.utils.fs import FS
@@ -10,15 +6,14 @@ from jnpr.junos.exception import *
 from lxml import etree
 from pprint import pprint
 import re
-import os, errno
+import os
+import errno
 import os.path
 import time
 import sys
 import socket
 
-#################
-### variables ###
-#################
+# variables #
 
 if len(sys.argv) > 2:
     print("Please only call me with one parameter")
@@ -27,74 +22,70 @@ if len(sys.argv) > 2:
 device_ip = sys.argv[1]
 
 try:
-       socket.inet_aton(device_ip)
+    socket.inet_aton(device_ip)
 except Exception as err:
-       print err
-       sys.exit(1)
+    print err
+    sys.exit(1)
 
-path = "/home/idelrio/scripts/snapshoter/";
+path = "/home/idelrio/scripts/snapshoter/"
 disable_cmds = path + "disable_cmds.txt"
 enable_syslog = path + "enable_syslog.txt"
-filename_snap = path +  "files/" + device_ip + ".snap"
-filerun = path  + device_ip + ".run"
-filelog = path + "/log/" + device_ip + "_" + time.strftime("%Y-%m-%d_%H:%M:%S") + ".log"
+filename_snap = path + "files/" + device_ip + ".snap"
+filerun = path + device_ip + ".run"
+filelog = path + "/log/" + device_ip + "_" + \
+    time.strftime("%Y-%m-%d_%H:%M:%S") + ".log"
 
-#################
-### functions ###
-#################
+# functions #
+
 
 def close():
 
-        file_log.close()
-        os.remove(filename_snap)
-        os.remove(filerun)
-	j_device.close()
+    file_log.close()
+    os.remove(filename_snap)
+    os.remove(filerun)
+    j_device.close()
+
 
 def log(text):
 
-        file_log.write(text)
+    file_log.write(text)
 
 
-def my_commit(file_path,device):
+def my_commit(file_path, device):
 
-	#load snippet configuration
-	cfg = Config(device)
+    # load snippet configuration
+    cfg = Config(device)
 
-	#lock candidate configuration
-	cfg.lock()
+    # lock candidate configuration
+    cfg.lock()
 
-	#load configuration
-	try:
-	       cfg.load(path=file_path, format="set", merge=True, ignore_warning=True)
-	except ConfigLoadError as e:
-    			if e.rpc_error['severity'] == 'warning':
-        			pass
-    			else:
-        			raise
+    # load configuration
+    try:
+        cfg.load(path=file_path, format="set", merge=True, ignore_warning=True)
+    except ConfigLoadError as e:
+        if e.rpc_error['severity'] == 'warning':
+            pass
+        else:
+            raise
 
-	#commit configuration
-	if cfg.commit():
-	       pass	
-	else:
-	       print "Failed to commit configuration.Aborting!"
-	       log("Failed to commit configuration.Aborting!")
-	       close()
-	       sys.exit(1)
+    # commit configuration
+    if cfg.commit():
+        pass
+    else:
+        print "Failed to commit configuration.Aborting!"
+        log("Failed to commit configuration.Aborting!")
+        close()
+        sys.exit(1)
+
+    # unlock configuration
+    cfg.unlock()
 
 
-	#unlock configuration
-	cfg.unlock()
+# BEGIN #
 
-
-#############
-### BEGIN ###
-#############
-
-### open log file ###                           
+# open log file #
 
 file_log = open(filelog, "w")
-
-### connect host ###
 
 print ""
 print "##########################"
@@ -102,45 +93,47 @@ print "### Auto-snapshot v1.0 ###"
 print "##########################"
 print ""
 
-### Is there any process that is targeting the same device already running? ###
+# Is there any process that is targeting the same device already running? #
 
 if os.path.exists(filerun):
-        print "-> There is another script instance targeting same device:",device_ip
-	log("-> There is another script instance targeting same device")
-	file_log.close()
-        sys.exit(1)
+    print "-> There is another script instance on same device", device_ip
+    log("-> There is another script instance on same device")
+    file_log.close()
+    sys.exit(1)
 
-print "-> Connecting to ",device_ip
+print "-> Connecting to ", device_ip
 
-j_device = Device(host=device_ip, user='user', password=pass')
-  
+j_device = Device(host=device_ip, user='user', password='pass')
+
 try:
-	j_device.open()
+    j_device.open()
 except Exception as err:
-       	print "Cannot connect to device:", err
-	close()
-	sys.exit(1)
+    print "Cannot connect to device:", err
+    close()
+    sys.exit(1)
 
-### increases device connection timeout ###
+# increases device connection timeout #
 
-j_device.timeout= 600
+j_device.timeout = 600
 
-### Is the device platform ACX1100 ACX2200 ACX2200 ? ###
+# Is the device platform ACX1100 ACX2200 ACX2200 ? #
 
-if j_device.facts['model'] == "ACX2200" or j_device.facts['model'] == "ACX1100" or j_device.facts['model'] == "ACX2100":
-        print "-> Device platform: ",j_device.facts['model']
-else: 	
-	print "-> (i) Platform device is not ACX1100/ACX2100/ACX2200"
-	log("-> (i) Platform device is neither ACX1100 nor ACX2200 or ACX2100")
-	close()
-	sys.exit(1)
+if j_device.facts['model'] == "ACX2200" \
+        or j_device.facts['model'] == "ACX1100" \
+        or j_device.facts['model'] == "ACX2100":
+    print "-> Device platform: ", j_device.facts['model']
+else:
+    print "-> (i) Platform device is not ACX1100/ACX2100/ACX2200"
+    log("-> (i) Platform device is neither ACX1100 nor ACX2200 or ACX2100")
+    close()
+    sys.exit(1)
 
 
-### Inform other scripts what device is the script targeting into. ###
+# Inform other scripts what device is the script targeting into. #
 
 frun = open(filerun, "w")
 
-### Looks for input/output errors from previous snapshot ###
+# Looks for input/output errors from previous snapshot #
 
 text_file_snap = open(filename_snap, "w")
 op = j_device.rpc.file_show(filename='/var/log/snapshot')
@@ -148,44 +141,44 @@ text_file_snap.write(etree.tostring(op))
 text_file_snap.close()
 
 with open(filename_snap) as f:
-        for line in f:
-                if re.search('Input/output error', line):
-                        print "-> (e) Device has Input/output error.Aborting snapshot!"
-			log("-> (e) Device has Input/output error.Aborting snapshot!")
-			close()
-			sys.exit(1)
+    for line in f:
+        if re.search('Input/output error', line):
+            print "-> (e) Device has Input/output error.Aborting snapshot!"
+            log("-> (e) Device has Input/output error.Aborting snapshot!")
+            close()
+            sys.exit(1)
 
 text_file_snap.close()
 
-### request system storage cleanup ### 
+# request system storage cleanup #
 
 print "-> Performing system storage cleanup, please be patient"
 
 fs = FS(j_device)
 fs.storage_cleanup()
 
-### Disables traceoptions + syslog ###
+# Disables traceoptions + syslog #
 
 print "-> Disabling syslog + traceoptions (when enabled)"
-my_commit(disable_cmds,j_device)
+my_commit(disable_cmds, j_device)
 
-### Take a snapshot ###
+# Take a snapshot #
 
 print "-> Snapshoting the device.."
 
-try: 
-	rsp = j_device.rpc.request_snapshot(slice='alternate',dev_timeout=600)
+try:
+    rsp = j_device.rpc.request_snapshot(slice='alternate', dev_timeout=600)
 except Exception as err:
-       print "--> (e) Error when snapshoting the device.You may need to replace device asap!", err
-       log("--> (e) Error when snapshoting the device.You may need to replace device asap!")
+    print "--> (e) Error when snapshoting the device!", err
+    log("--> (e) Error when snapshoting the device!")
 
 
-### enables syslog + keep traceoptions disabled ###
+# enables syslog + keep traceoptions disabled #
 
 print "-> Re-enabling only syslog!"
-my_commit(enable_syslog,j_device)
+my_commit(enable_syslog, j_device)
 
-### shows contents of dumpdates file ### START
+# shows contents of dumpdates file #
 
 dumpd_file = j_device.rpc.file_show(filename='/etc/dumpdates')
 dumpf = etree.tostring(dumpd_file, encoding='utf8', method='text')
@@ -195,7 +188,6 @@ print dumpf
 print "-> The End"
 log(dumpf)
 
-### close ###
+# close #
 
 close()
-
