c8f57982ffe077132dcfcaad413878428dc733a3
Jessica Garrison <jgarrison@juniper.net>
2017-11-06 11:23:40 -0800

    Add copyright

--- pyez/PyEZ_Cookbook_2017/23-Configuration_Audit_Using_XML_Schema/validate_vmx1_config.py
+++ pyez/PyEZ_Cookbook_2017/23-Configuration_Audit_Using_XML_Schema/validate_vmx1_config.py
@@ -1,3 +1,5 @@
+# Copyright 2017, Juniper Networks Pvt Ltd.
+# All rights reserved
 #!/opt/local/bin/python
                                                                                 
 import lxml                                                                     
